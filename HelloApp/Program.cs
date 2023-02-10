500547397a62862c7cd7405b91f8fef34e789a17
oleg <olegfide@mail,ru>
2023-02-10 14:58:25 +0500

    overload Run with html statement Add project files.

---
+++ HelloApp/Program.cs
@@ -0,0 +1,11 @@
+var builder = WebApplication.CreateBuilder();
+var app = builder.Build();
+
+app.Run(async (context) =>
+{
+    var response = context.Response;
+    response.ContentType = "text/html; charset=utf-8";
+    await response.WriteAsync("<h2>Hello METANIT.COM</h2><h3>Welcome to ASP.NET Core</h3>");
+});
+
+app.Run();
