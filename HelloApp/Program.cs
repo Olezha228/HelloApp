00435d950798f27b60c839e37b1a858b7023f5dd
oleg <olegfide@mail,ru>
2023-02-12 21:46:19 +0500

    RequestHeaders

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -3,11 +3,17 @@ var app = builder.Build();
 
 app.Run(async (context) =>
 {
-    var response = context.Response;
-    response.ContentType = "text/html; charset=utf-8";
-    await response.WriteAsync("<h2>Hello METANIT.COM</h2><h3>Welcome to ASP.NET Core</h3>");
+    context.Response.ContentType = "text/html; charset=utf-8";
+    var stringBuilder = new System.Text.StringBuilder("<table>");
+
+    foreach (var header in context.Request.Headers)
+    {
+        stringBuilder.Append($"<tr><td>{header.Key}</td><td>{header.Value}</td></tr>");
+    }
+    stringBuilder.Append("</table>");
+    await context.Response.WriteAsync(stringBuilder.ToString());
 });
 
-// this is master branch
-
 app.Run();
+
+// this is master branch
