c453cc9785f7a0b6d9bf92f5da29b1c6c4556e29
oleg <olegfide@mail,ru>
2023-02-12 23:30:07 +0500

    new test project

---
+++ MathTests/CalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using Math;
+using NUnit.Framework;
+
+// ReSharper disable ConvertToConstant.Local
+
+namespace MathTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Add_WhenPositive_ValidExpected()
+        {
+            var first = 5;
+            var second = 10;hjkhk
+
+            var expected = 15;
+            var result = Calculator.Add(first, second);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
